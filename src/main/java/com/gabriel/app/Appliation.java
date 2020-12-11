c2b7a5677dbd8c83951911499c67a0dc34cd897b
Gabriel <gabrielhondacba@gmail.com>
2020-12-11 10:05:40 -0400

    adiciona classe principal

---
+++ src/main/java/com/gabriel/app/Appliation.java
@@ -0,0 +1,12 @@
+package com.gabriel.app;
+
+/**
+ * @author daohn on 11/12/2020
+ * @project jsf-maven-template
+ */
+public class Appliation {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
