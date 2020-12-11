ef7091f84063cc88edc9285cfbf5b7e782ca7851
Gabriel <gabrielhondacba@gmail.com>
2020-12-11 10:06:26 -0400

    corrige nome da classe

--- src/main/java/com/gabriel/app/Application.java
+++ src/main/java/com/gabriel/app/Application.java
@@ -4,7 +4,7 @@ package com.gabriel.app;
  * @author daohn on 11/12/2020
  * @project jsf-maven-template
  */
-public class Appliation {
+public class Application {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
